9a38d127ce9ef4bd03be1369d46a76df82dc3167
reljatrkulja <reljatrkulja1@gmail.com>
2022-04-29 14:47:00 +0200

    Prva promena

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+	public static void main(String[] args) {
+		System.out.println("ovo radi sad");
+		System.out.println("ovo radi sad opet");
+		System.out.println("Jos jednom da probam");
+	}
+}
