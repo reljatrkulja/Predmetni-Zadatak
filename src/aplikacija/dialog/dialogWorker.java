67f8850c79da9896090f4c3860e6fe57da437cb6
ai4-2019 <petrovski.ai4.2019@uns.ac.rs>
2022-06-12 16:20:18 +0200

    napravio novi modalni dialog
    Korsisnik sada moze da unese tekst i podatke

--- src/aplikacija/dialog/dialogWorker.java
+++ src/aplikacija/dialog/dialogWorker.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.sql.Date;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -25,163 +26,146 @@ import aplikacija.model.Softver;
 import aplikacija.model.RadnoMesto;
 import aplikacija.gui.Frame1;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import javax.swing.JFormattedTextField;
 
+import java.awt.Frame;
+import javax.swing.JDialog;
 
 public class dialogWorker extends JDialog implements ActionListener {
 
-	private static final long serialVersionUID = -514322840576145538L;
-
+	private static final long serialVersionUID = 3591599721565020284L;
+	
 	public static final int OK = 0;
 	public static final int CANCEL = 1;
-
+	
 	private int mode = 1;
-	private JTextArea area;
+	private JTextField field;
 
 	public dialogWorker(Frame parent, String title, boolean modal) {
-		super(parent, "Add Worker", true);
+		super(parent, title, modal);
 		mode = dialogWorker.CANCEL;
 		setLayout(new BorderLayout());
 
-		setSize(450, 250);
+		setSize(500, 500);
 		setLocationRelativeTo(parent);
-		
-		JPanel polja = new JPanel(new GridLayout(6, 2));
 
+		// Deo za unos zaposlenog
+		JPanel polja = new JPanel(new GridLayout(12, 2));
+
+		////////////////////// Ime
 		JLabel ime = new JLabel("Ime:");
 		polja.add(ime);
-		String imeString = new String(ime.toString());
-	
+		
 		JTextField imePolje = new JTextField();
 		polja.add(imePolje);
-	
-	
+		
+		String imeString = new String(ime.toString());
+		
+		////////////////////// Prezime
 		JLabel prezime = new JLabel("Prezime:");
 		polja.add(prezime);
-		String prezimeString = new String(prezime.toString());
 	
 		JTextField prezimePolje = new JTextField();
 		polja.add(prezimePolje);
 		
+		String prezimeString = new String(prezime.toString());
 		
-		//////////////////////////////////////////////////////////Jmbg
+        ////////////////////// JMBG
 		JLabel jmbg = new JLabel("JMBG:");
 		polja.add(jmbg);
-		String jmbgString = new String(jmbg.toString());
-	
+		
 		JTextField jmbgPolje = new JTextField();
 		polja.add(jmbgPolje);
 		
+		String jmbgString = new String(jmbg.toString());
 		
-		//////////////////////////////////////////////////////////Datum rodjenja
-		//Dan
-		JLabel dan = new JLabel("Dan:");
-		polja.add(dan);
-		String danString = new String(dan.toString());
-		int danInt = Integer.parseInt(danString);
+		////////////////////// Datum rodjenja
+		JLabel datumRodjenja = new JLabel("Datum rodjenja:");
+		polja.add(datumRodjenja);
 		
-		JTextField danPolje = new JTextField();
-		polja.add(danPolje);
-		
-		//Mesec
-		JLabel mesec = new JLabel("Mesec:");
-		polja.add(mesec);
-		String mesecString = new String(mesec.toString());
-		int mesecInt = Integer.parseInt(mesecString);
-		
-		JTextField mesecPolje = new JTextField();
-		polja.add(mesecPolje);
-		
-		//Godina
-		JLabel godina = new JLabel("Godina:");
-		polja.add(godina);
-		String godinaString = new String(godina.toString());
-		int godinaInt = Integer.parseInt(godinaString);
-		
-		JTextField godinaPolje = new JTextField();
-		polja.add(godinaPolje);
-	   
-		
-		Date datumRodjenja = new Date(danInt, mesecInt, godinaInt);
+		JTextField datumPolje = new JTextField();
+		polja.add(datumPolje);
 		
 		
-		//JLabel datumRodjenja = new JLabel("Datum rodjenja:");
-		//polja.add(datumRodjenja);
-		//Date datumRodjenja1;
-		//datumRodjenja1.setDatumRodjenja(datumRodjenja);
-	
-		//JTextField datumRodjenjaPolje = new JTextField();
-		//polja.add(datumRodjenjaPolje);
-		
-		
-		//////////////////////////////////////////////////////////Email
+		////////////////////// Email
 		JLabel email = new JLabel("Email:");
 		polja.add(email);
-		String emailString = new String(email.toString());
-	
+		
 		JTextField emailPolje = new JTextField();
 		polja.add(emailPolje);
 		
+		String emailString = new String(email.toString());
 		
-		//////////////////////////////////////////////////////////Adresa
+		///////////////////// Adresa
 		//Broj ulice
-		JLabel brojUlice = new JLabel("Broj ulice:");
+		JLabel brojUlice = new JLabel("ADRESA - Broj ulice:");
 		polja.add(brojUlice);
-		String brojUliceString = new String(brojUlice.toString());
-		int brojInt = Integer.parseInt(brojUliceString);
-	
+		
 		JTextField brojUlicePolje = new JTextField();
 		polja.add(brojUlicePolje);
+		
+		String brojUliceString = new String(brojUlice.toString());
 		
 		//Naziv ulice
 		JLabel nazivUlice = new JLabel("Naziv ulice:");
 		polja.add(nazivUlice);
-		String nazivUliceString = new String(nazivUlice.toString());
 		
 		JTextField nazivUlicePolje = new JTextField();
 		polja.add(nazivUlicePolje);
 		
+		String nazivUliceString = new String(nazivUlice.toString());
+		
 		//Grad
 		JLabel grad = new JLabel("Grad:");
 		polja.add(grad);
-		String gradString = new String(grad.toString());
 		
 		JTextField gradPolje = new JTextField();
 		polja.add(gradPolje);
 		
+		String gradString = new String(grad.toString());
+		
 		Adresa adresa = new Adresa();
-		adresa.setBroj(brojInt);
+		adresa.setBroj(brojUliceString);
 		adresa.setUlica(nazivUliceString);
 		adresa.setGrad(gradString);
 		
-		
-		//////////////////////////////////////////////////////////Softver
+		//////////////////////////////Softver
 		JLabel softver = new JLabel("Softver:");
 		polja.add(softver);
-		String softverString = new String(softver.toString());
-	
+		
 		JTextField softverPolje = new JTextField();
 		polja.add(softverPolje);
 		
+		String softverString = new String(softver.toString());
 		Softver softverNaziv = new Softver();
 		softverNaziv.setNaziv(softverString);
 		
+		///////////////////////// /Radno mesto
+		JLabel radnoMesto = new JLabel("Radno mesto:");
+		polja.add(radnoMesto);
 		
-		//////////////////////////////////////////////////////////Radno mesto
-		//JLabel radnoMesto = new JLabel("Radno mesto:");
-		//polja.add(radnoMesto);
-		//JComboBox<RadnoMesto> RadnoMesto = new JComboBox<RadnoMesto>(RadnoMesto.values()); 
-
+		JComboBox radnoMestoPolje = new JComboBox<RadnoMesto>(RadnoMesto.values());
+		polja.add(radnoMestoPolje);
+		
+		//radnoMesto.getRadnoMesto(radnoMestoPolje);
+		
+		// Deo za dugmad
 		JPanel panCommands = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		
 		JButton btnOk = new JButton("OK");
 		btnOk.addActionListener(this);
+		panCommands.add(btnOk);
+		
 		JButton btnCancel = new JButton("CANCEL");
 		btnCancel.addActionListener(this);
-
-		panCommands.add(btnOk);
 		panCommands.add(btnCancel);
-
-		add(new JScrollPane(area), BorderLayout.CENTER);
+		
+		add(polja, BorderLayout.NORTH);
 		add(panCommands, BorderLayout.SOUTH);
+		
+		
 		
 		btnOk.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -189,11 +173,11 @@ public class dialogWorker extends JDialog implements ActionListener {
 				user.setIme(imeString);
 				user.setPrezime(prezimeString);
 				user.setJmbg(jmbgString);
-				user.setDatumRodjenja(datumRodjenja);
+				//user.setDatumRodjenja(datumRodjenja);
 				user.setEmail(emailString);
 				user.setAdresa(adresa);
 				user.setSoftver(softverNaziv);
-				//user.setRadnoMesto(RadnoMesto); ?
+				//user.setRadnoMesto(radnoMesto);
 				
 				/*if(Zaposleni.register(user)) {
 					Frame1.setView(new Frame1());
@@ -204,35 +188,38 @@ public class dialogWorker extends JDialog implements ActionListener {
 				
 			}
 		});
-		
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		if (arg0.getActionCommand().equals("OK")) {
 			mode = dialogWorker.OK;
+			
 		} else {
 			mode = dialogWorker.CANCEL;
 		}
 		setVisible(false);
-
 	}
-
+	
 	public int getMode() {
 		return mode;
 	}
-
+	
 	public void setMode(int mode) {
 		this.mode = mode;
 	}
+	
 
-	public JTextArea getArea() {
-		return area;
+	public JTextField getArea() {
+		return field;
 	}
 
-	public void setArea(JTextArea area) {
-		this.area = area;
+	public void setField(JTextField field) {
+		this.field = field;
 	}
-
+	
 }
+
+
+
 
