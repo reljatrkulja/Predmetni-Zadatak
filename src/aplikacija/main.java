110aa973a42b23c0ba1b6128619191daba07a139
gabajoe <g2gaborov@gmail.com>
2022-06-17 19:53:06 +0200

    Tabelarni prikaz zaposlenih i softvera

--- src/aplikacija/main.java
+++ src/aplikacija/main.java
@@ -1,9 +1,11 @@
 package aplikacija;
 
 import aplikacija.gui.Frame1;
+import aplikacija.utils.Utils;
 
 public class main {
 	public static void main(String[] args) {
+		Utils.popuniListe();
 		Frame1 frame = new Frame1();
 		frame.setVisible(true); // Prozor je inicijalno nevidljiv
 	}
